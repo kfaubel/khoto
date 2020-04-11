a4a061d116771e83b4a77fbf150b0eff493bf748
Ken Faubel <ken@faubel.org>
2020-04-11 12:11:38 -0400

    Updates

---
+++ precache-manifest.4c68385eaff016b08ae5e08cca60b34d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ab29449d8df27a2959c06c0f3803f7",
+    "url": "/khoto/index.html"
+  },
+  {
+    "revision": "a5198475a00729b1d468",
+    "url": "/khoto/static/css/main.9ab2e04f.chunk.css"
+  },
+  {
+    "revision": "3d2f14b0bee9d331f033",
+    "url": "/khoto/static/js/2.75af542d.chunk.js"
+  },
+  {
+    "revision": "9617c35fefb3909e96d4735aba67fb26",
+    "url": "/khoto/static/js/2.75af542d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5198475a00729b1d468",
+    "url": "/khoto/static/js/main.126539a6.chunk.js"
+  },
+  {
+    "revision": "9228f8252be002dd9609",
+    "url": "/khoto/static/js/runtime-main.d0b4d86d.js"
+  }
+]);
